03e2bd76fada0707542a4925099de983ff6aff18
Marius Sarca <marius.sarca@gmail.com>
2016-08-15 00:05:18 +0300

    Bugfix

--- lib/Application.php
+++ lib/Application.php
@@ -719,12 +719,11 @@ class Application implements DefaultCollectorInterface
         }
 
         if (!$this->info->installMode()) {
+            Model::setApplication($this);
             $this->getBootstrapInstance()->bootstrap($this);
             $this->emit('system.init');
             return $this;
         }
-
-        Model::setApplication($this);
 
         $composer = $this->getComposerCLI()->getComposer();
         $generator = $composer->getAutoloadGenerator();
