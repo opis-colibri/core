03fa708c1811537e19bf868fbff25a7e4a00a9a6
Marius Sarca <marius.sarca@gmail.com>
2016-08-16 01:10:30 +0300

    Added base classes

--- lib/Base/Command.php
+++ lib/Base/Command.php
@@ -1,9 +1,6 @@
 <?php
 /* ===========================================================================
- * Opis Project
- * http://opis.io
- * ===========================================================================
- * Copyright 2014-2016 Marius Sarca
+ * Copyright 2013-2016 The Opis Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,31 +15,11 @@
  * limitations under the License.
  * ============================================================================ */
 
-namespace Opis\Colibri;
+namespace Opis\Colibri\Base;
 
-use Opis\Colibri\Components\ApplicationTrait;
+use Symfony\Component\Console\Command\Command as ConsoleCommand;
 
-abstract class Controller
+class Command extends ConsoleCommand
 {
-    use ApplicationTrait;
 
-    /** @var  Application */
-    protected $app;
-
-    /**
-     * Controller constructor.
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
-    }
-
-    /**
-     * @return Application
-     */
-    protected function getApp(): Application
-    {
-        return $this->app;
-    }
 }
