7c99e333a8ef072ccdcd3b28c637fbeb1ae70c70
Marius Sarca <marius.sarca@gmail.com>
2017-09-18 18:28:36 +0300

    Modified helper functions

--- res/functions.php
+++ res/functions.php
@@ -38,13 +38,7 @@ use Psr\Log\LoggerInterface;
  */
 function app(): Application
 {
-    static $app;
-
-    if($app === null){
-        $app = Application::getInstance();
-    }
-
-    return $app;
+    return Application::getInstance();
 }
 
 /**
@@ -53,9 +47,7 @@ function app(): Application
  */
 function cache(string $storage = 'default'): CacheInterface
 {
-    static $cache = [];
-
-    return $cache[$storage] ?? (app()->getCache($storage));
+    return Application::getInstance()->getCache($storage);
 }
 
 /**
@@ -64,9 +56,7 @@ function cache(string $storage = 'default'): CacheInterface
  */
 function config(string $storage = 'default'): ConfigInterface
 {
-    static $config = [];
-
-    return $config[$storage] ?? (app()->getConfig($storage));
+    return Application::getInstance()->getConfig($storage);
 }
 
 /**
@@ -76,13 +66,7 @@ function config(string $storage = 'default'): ConfigInterface
  */
 function make(string $abstract, array $arguments = [])
 {
-    static $container;
-
-    if($container === null){
-        $container = app()->getContainer();
-    }
-
-    return $container->make($abstract, $arguments);
+    return Application::getInstance()->getContainer()->make($abstract, $arguments);
 }
 
 /**
@@ -90,7 +74,7 @@ function make(string $abstract, array $arguments = [])
  */
 function generateCSRFToken(): string
 {
-    return app()->getCSRFToken()->generate();
+    return Application::getInstance()->getCSRFToken()->generate();
 }
 
 /**
@@ -99,7 +83,7 @@ function generateCSRFToken(): string
  */
 function validateCSRFToken(string $token): bool
 {
-    return app()->getCSRFToken()->validate($token);
+    return Application::getInstance()->getCSRFToken()->validate($token);
 }
 
 /**
@@ -108,9 +92,7 @@ function validateCSRFToken(string $token): bool
  */
 function connection(string $name = 'default'): DBConnection
 {
-    static $connection = [];
-
-    return $connection[$name] ?? ($connection[$name] = app()->getConnection($name));
+    return Application::getInstance()->getConnection($name);
 }
 
 /**
@@ -119,9 +101,7 @@ function connection(string $name = 'default'): DBConnection
  */
 function db(string $connection = 'default'): Database
 {
-    static $db = [];
-
-    return $db[$connection] ?? ($db[$connection] = app()->getDatabase($connection));
+    return Application::getInstance()->getDatabase($connection);
 }
 
 /**
@@ -130,9 +110,7 @@ function db(string $connection = 'default'): Database
  */
 function schema(string $connection = 'default'): Schema
 {
-    static $schema = [];
-
-    return $schema[$connection] ?? ($schema[$connection] = app()->getSchema($connection));
+    return Application::getInstance()->getSchema($connection);
 }
 
 /**
@@ -188,7 +166,7 @@ function transaction(callable $callback, array $options = [])
  */
 function entity(string $class, string $connection = 'default'): EntityQuery
 {
-    return entityManager($connection)->query($class);
+    return Application::getInstance()->getEntityManager($connection)->query($class);
 }
 
 /**
@@ -197,8 +175,7 @@ function entity(string $class, string $connection = 'default'): EntityQuery
  */
 function entityManager(string $connection = 'default'): EntityManager
 {
-    static $em = [];
-    return $em[$connection] ?? ($em[$connection] = app()->getEntityManager($connection));
+    return Application::getInstance()->getEntityManager($connection);
 }
 
 /**
@@ -208,7 +185,7 @@ function entityManager(string $connection = 'default'): EntityManager
  */
 function emit(string $event, bool $cancelable = false): Event
 {
-    return dispatch(new Event($event, $cancelable));
+    return Application::getInstance()->getEventTarget()->dispatch(new Event($event, $cancelable));
 }
 
 /**
@@ -217,13 +194,7 @@ function emit(string $event, bool $cancelable = false): Event
  */
 function dispatch(Event $event): Event
 {
-    static $target;
-
-    if($target === null){
-        $target = app()->getEventTarget();
-    }
-
-    return $target->dispatch($event);
+    return Application::getInstance()->getEventTarget()->dispatch($event);
 }
 
 /**
@@ -231,13 +202,7 @@ function dispatch(Event $event): Event
  */
 function request(): Request
 {
-    static $request;
-
-    if($request === null){
-        $request = app()->getHttpRequest();
-    }
-
-    return $request;
+    return Application::getInstance()->getHttpRequest();
 }
 
 /**
@@ -289,7 +254,7 @@ function accessDenied($body = null): AccessDeniedResponse
  */
 function validator(): Validator
 {
-    return app()->getValidator();
+    return Application::getInstance()->getValidator();
 }
 
 /**
@@ -297,13 +262,7 @@ function validator(): Validator
  */
 function info(): AppInfo
 {
-    static $info;
-
-    if($info === null){
-        $info = app()->getAppInfo();
-    }
-
-    return $info;
+    return Application::getInstance()->getAppInfo();
 }
 
 /**
@@ -312,9 +271,7 @@ function info(): AppInfo
  */
 function logger(string $logger = 'default'): LoggerInterface
 {
-    static $log = [];
-
-    return $log[$logger] ?? ($log[$logger] = app()->getLog($logger));
+    return Application::getInstance()->getLog($logger);
 }
 
 /**
@@ -323,9 +280,7 @@ function logger(string $logger = 'default'): LoggerInterface
  */
 function session(string $storage = 'default'): Session
 {
-    static $session = [];
-
-    return $session[$storage] ?? ($session[$storage] =  app()->getSession($storage));
+    return Application::getInstance()->getSession($storage);
 }
 
 /**
@@ -337,12 +292,7 @@ function session(string $storage = 'default'): Session
  */
 function v(string $name, $default = null)
 {
-    static $var;
-
-    if($var === null){
-        $var = app()->getVariables();
-    }
-
+    $var = Application::getInstance()->getVariables();
     return array_key_exists($name, $var) ? $var[$name] : $default;
 }
 
@@ -355,13 +305,7 @@ function v(string $name, $default = null)
  */
 function r(string $text, array $placeholders): string
 {
-    static $placeholder;
-
-    if($placeholder === null){
-        $placeholder = app()->getPlaceholder();
-    }
-
-    return $placeholder->replace($text, $placeholders);
+    return Application::getInstance()->getPlaceholder()->replace($text, $placeholders);
 }
 
 /**
@@ -374,13 +318,7 @@ function r(string $text, array $placeholders): string
  */
 function t(string $sentence, array $placeholders = [], string $lang = null): string
 {
-    static $translator;
-
-    if($translator === null){
-        $translator = app()->getTranslator();
-    }
-
-    return $translator->translate($sentence, $placeholders, $lang);
+    return Application::getInstance()->getTranslator()->translate($sentence, $placeholders, $lang);
 }
 
 /**
@@ -452,13 +390,7 @@ function view(string $name, array $arguments = []): View
  */
 function render($view): string
 {
-    static $viewApp;
-
-    if($viewApp === null){
-        $viewApp = app()->getViewApp();
-    }
-
-    return $viewApp->render($view);
+    return Application::getInstance()->getViewApp()->render($view);
 }
 
 /**
