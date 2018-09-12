008da64526ba237501d625fb82af688e028496e1
Marius Sarca <marius.sarca@gmail.com>
2018-09-12 16:00:40 +0300

    Added meta info

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,4 +1,46 @@
 <?php
 namespace PHPSTORM_META {
     override(\Opis\Colibri\Functions\make(0), type(0));
+
+    override(\Opis\Colibri\Collector\Manager::collect(0), map([
+        'routes' => \Opis\Colibri\Routing\HttpRouteCollection::class,
+        'router-globals' => \Opis\Colibri\Serializable\RouterGlobals::class,
+        'middleware' => \Opis\Colibri\Serializable\ClassList::class,
+        'views' => \Opis\View\RouteCollection::class,
+        'contracts' => \Opis\Colibri\Container::class,
+        'connections' => \Opis\Colibri\Serializable\ConnectionList::class,
+        'event-handlers' => \Opis\Events\RouteCollection::class,
+        'view-engines' => \Opis\Colibri\Serializable\ViewEngineResolver::class,
+        'cache-drivers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'session-handlers' => \Opis\Colibri\Serializable\CallbackList::class,
+        'config-drivers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'translations' => \Opis\Colibri\Serializable\Translations::class,
+        'translation-filters' => \Opis\Colibri\Serializable\ClassList::class,
+        'commands' => \Opis\Colibri\Serializable\CallbackList::class,
+        'loggers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'loggers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'asset-handlers' => \Opis\Colibri\Serializable\CallbackList::class,
+        //'validators' => [],
+    ]));
+
+    override(\Opis\Colibri\Functions\collect(0), map([
+        'routes' => \Opis\Colibri\Routing\HttpRouteCollection::class,
+        'router-globals' => \Opis\Colibri\Serializable\RouterGlobals::class,
+        'middleware' => \Opis\Colibri\Serializable\ClassList::class,
+        'views' => \Opis\View\RouteCollection::class,
+        'contracts' => \Opis\Colibri\Container::class,
+        'connections' => \Opis\Colibri\Serializable\ConnectionList::class,
+        'event-handlers' => \Opis\Events\RouteCollection::class,
+        'view-engines' => \Opis\Colibri\Serializable\ViewEngineResolver::class,
+        'cache-drivers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'session-handlers' => \Opis\Colibri\Serializable\CallbackList::class,
+        'config-drivers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'translations' => \Opis\Colibri\Serializable\Translations::class,
+        'translation-filters' => \Opis\Colibri\Serializable\ClassList::class,
+        'commands' => \Opis\Colibri\Serializable\CallbackList::class,
+        'loggers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'loggers' => \Opis\Colibri\Serializable\StorageCollection::class,
+        'asset-handlers' => \Opis\Colibri\Serializable\CallbackList::class,
+        //'validators' => [],
+    ]));
 }
