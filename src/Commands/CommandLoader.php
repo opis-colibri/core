ec8de110bfd09028b7d278a2746ab174fed149ae
sorinsarca <sarca_sorin@hotmail.com>
2019-11-15 15:03:32 +0200

    Updated

--- src/Commands/CommandLoader.php
+++ src/Commands/CommandLoader.php
@@ -54,7 +54,6 @@ class CommandLoader implements CommandLoaderInterface
                 'assets:build' => Assets\Build::class,
                 'assets:yarn' => Assets\Yarn::class,
                 'spa:build' => Spa\Build::class,
-                'session:gc'=> Session\GC::class,
             ] + $classes;
 
         $this->builders = array_filter($builders, function ($callable) {
